d09b02efbef06c003aa13f47b4afc6de3a5fb739
Rens Laros <renslaros@MacBook-Pro-van-Apollo.local>
2022-01-21 23:50:29 +0300

    RL

--- contracts/CosmicMonkeyClub.sol
+++ contracts/CosmicMonkeyClub.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-// Written by LEDK For the Cosmic Monkey CLub
+// Made with ❤ by Rens L & LEDK
+// Email: info@renslaros.com
+// Twitter: @humanrens 
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -9,57 +11,59 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract CosmicMonkeyClub is ERC721Enumerable, Ownable, VRFConsumerBase{
     using Strings for uint256;
-    bytes32 private root;
     string public baseURI;
+    string public contractUrl = "";
+    bytes32 private merkleRoot;
+    bytes32 internal keyHash;
     uint256 public maxSupply = 10000;
     uint256 public maxPresaleSupply = 10000;
+    uint256 public presalePrice = 0.088 ether;
+    uint256 public publicPrice = 0.1 ether;
     uint256 internal maxPublicSaleMint = 30;
     uint256 internal maxMintPerTransaction = 6;
     uint256 internal maxPresaleMint = 4;
-    uint256 public presalePrice = 0.088 ether;
-    uint256 public publicPrice = 0.1 ether;
-    bool public isPublicSale = false;
-    bool public isPresale = false;
-    address[] public ogList;
-    mapping(address => uint256) internal addressMintedBalance;
-    bytes32 internal keyHash;
     uint256 internal fee;
     uint256 public randomResult;
-
-    constructor(string memory name, string memory symbol,string memory _initBaseURI) ERC721(name, symbol) VRFConsumerBase(
-            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
-            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
-        ) 
-        {
+    bool public isPublicSale = false;
+    bool public isPresale = false;
+    bool public isRevealed = false;
+    address[] public ogList;
+    mapping(address => uint256) internal addressMintedBalance;
+    constructor(string memory name, string memory symbol,string memory _initBaseURI) 
+    ERC721(name, symbol) 
+    VRFConsumerBase(0x8C7382F9D8f56b33781fE506E897a4F1e2d17255,0x326C977E6efc84E512bB9C30f76E30c160eD06FB) 
+    {
         setBaseURI(_initBaseURI);
-        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        // Chainlink VRF keyhash + fee ( different per network )
+        keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
         fee = 0.1 * 10 ** 18; 
-        }
-
-    function getRandomNumber() public returns (bytes32 requestId) {
+    }
+    // Request random number from Chainlink VRF
+    function getRandomNumber() public onlyOwner returns (bytes32 requestId) {
+        require(randomResult == 0);
         require(LINK.balanceOf(address(this)) >= fee);
         return requestRandomness(keyHash, fee);
     }
-
+    // randomness callback function used by VRF coordinator
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
+        randomResult = randomness + 1;
     }
-
-//PUBLIC
+    
     //Presale Mint
-
     function mintPresale(uint256 _amount,address account, bytes32[] calldata proof) external payable {
         
         require(_amount > 0);
+        require(randomResult > 0);
         require(_amount  <= maxPresaleMint);
         if (msg.sender != owner()) {
             require(isPresale,"Presale isn't live");
-            require(isWhiteListed(account, proof), "Not on the whitelist");
-            require(msg.value >= (presalePrice * _amount), "We are not cheap");
-            require((addressMintedBalance[msg.sender] + _amount) <= maxPresaleMint, "You can't exceed 4 NFT's in Presale.");     
+            require(isWhiteListed(account, proof), "You're not whitelisted");
+            require(msg.value >= (presalePrice * _amount), "Amount to low");
+            require((addressMintedBalance[msg.sender] + _amount) <= maxPresaleMint, "Max. 4 NFT's in Presale.");     
         }
         for (uint256 i = 1; i <= _amount; i++) {
-            _safeMint(msg.sender, totalSupply() + i);
+            //_safeMint(msg.sender, totalSupply() + i);
+            _safeMint(msg.sender, ((randomResult + totalSupply()) % maxSupply) + i);
             addressMintedBalance[msg.sender]++;
             if(addressMintedBalance[msg.sender] == 4){
                 ogList.push(msg.sender);
@@ -68,24 +72,39 @@ contract CosmicMonkeyClub is ERC721Enumerable, Ownable, VRFConsumerBase{
     }
 
     //Public Mint
-
      function mintPublicSale(uint256 _amount) external payable {
         require(_amount > 0 );
-        require(_amount  <= maxMintPerTransaction);
+        require(randomResult > 0);
+        require(_amount  <= maxMintPerTransaction, "Exceeding max. mint amount per tx.");
         require(totalSupply() + _amount <= maxSupply,"SOLD OUT");
         if (msg.sender != owner()) {
             require(isPublicSale,"Public Sale isn't live");
             require(msg.value >= (publicPrice * _amount), "We are not cheap");
             require(_amount <= maxPublicSaleMint);    
-            require((addressMintedBalance[msg.sender] + _amount) <= maxPublicSaleMint, "You can't exceed 30 NFT's.");
+            require((addressMintedBalance[msg.sender] + _amount) <= maxPublicSaleMint, "Max. 30 NFT's.");
         }
         for (uint256 i = 1; i <= _amount; i++) {
             addressMintedBalance[msg.sender]++;
-            _safeMint(msg.sender, totalSupply() + i);
+            //_safeMint(msg.sender, totalSupply() + i);
+            _safeMint(msg.sender, ((randomResult + totalSupply()) % maxSupply) + i);
             }
     }
 
-    // Token's Uri Format
+    //Merkle Proof
+    function isWhiteListed(address account, bytes32[] calldata proof) internal view returns(bool) {
+        return _verify(_leaf(account), proof);
+    }
+
+    function _leaf(address account) internal pure returns(bytes32) {
+        return keccak256(abi.encodePacked(account));
+    }
+
+    function _verify(bytes32 leaf, bytes32[] memory proof) internal view returns(bool) {
+        return MerkleProof.verify(proof, merkleRoot, leaf);
+    }
+
+
+    // URI's
      function tokenURI(uint256 tokenId)
         public
         view
@@ -97,53 +116,34 @@ contract CosmicMonkeyClub is ERC721Enumerable, Ownable, VRFConsumerBase{
         _exists(tokenId),
         "ERC721Metadata: URI query for nonexistent token"
         );
-        
-        return bytes(_baseURI()).length > 0
-            ? string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json"))
-            : "";
-    }
-
-    function contractURI() public pure returns (string memory) {
-        return "https://gateway.pinata.cloud/ipfs/QmQ1JjUVh3KLHQjQbqFcgWoMy5gM3tqY1ZhRdUnin5DrSF";
-    }
-
-   
-
-    //Merkl Proof
-    function isWhiteListed(address account, bytes32[] calldata proof) internal view returns(bool) {
-        return _verify(_leaf(account), proof);
-    }
-
-    function _leaf(address account) internal pure returns(bytes32) {
-        return keccak256(abi.encodePacked(account));
-    }
-
-    function _verify(bytes32 leaf, bytes32[] memory proof) internal view returns(bool) {
-        return MerkleProof.verify(proof, root, leaf);
+        if (isRevealed) {
+            return bytes(_baseURI()).length > 0
+                ? string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json"))
+                : "";
+        }
+        return _baseURI();
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
     }
 
-//OnlyOWNER
-
-    function setBaseURI(string memory _newBaseURI) internal onlyOwner {
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
-    } 
-
-    
-    function giftNftToAddress(address _sendNftsTo, uint256 _amount)
-        external
-        onlyOwner
-    { 
-        for (uint256 i = 1; i <= _amount; i++)
-            _safeMint(_sendNftsTo,totalSupply() + i);
     }
 
+    function contractURI() public view returns (string memory) {
+        return contractUrl;
+    }
+
+    function setContractUri(string memory _newContractUrl) public onlyOwner {
+        contractUrl = _newContractUrl;
+    }
+
+    // Sale Controls
     function StartPresale(bytes32 _merkleRoot) external onlyOwner{
+        merkleRoot = _merkleRoot;
         isPresale = true;
-        root = _merkleRoot;
     }
 
     function StopPresale() external onlyOwner{
@@ -152,6 +152,24 @@ contract CosmicMonkeyClub is ERC721Enumerable, Ownable, VRFConsumerBase{
  
     function StartPublicSale() external onlyOwner{
         isPublicSale = true;
+    } 
+
+    function setRevealed() external onlyOwner {
+        isRevealed = true;
     }
 
+    // Gifting
+    function giftNftToAddress(address _sendNftsTo, uint256 _amount)
+        external
+        onlyOwner
+    {
+        require(randomResult > 0); 
+        for (uint256 i = 1; i <= _amount; i++)
+            //_safeMint(_sendNftsTo,totalSupply() + i);
+            _safeMint(msg.sender, ((randomResult + totalSupply()) % maxSupply) + i);
+    }
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
 }
